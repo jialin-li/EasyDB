14076ecd9f9300182af27bb6717b0417da0b9725
William Lin <wlsaidhi@gmail.com>
2018-01-29 14:37:19 -0600

    initial main.go

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, _ := reader.ReadString('\n')
+
+		//substrings := strings.Split(text, " ")
+
+		switch strs := strings.Split(text, " "); strs[0] {
+		case "joinServer":
+			fmt.Println(strs[1])
+		case "killServer":
+			fmt.Println(strs[1])
+		case "joinClient":
+			fmt.Println(strs[1])
+		case "breakConnection":
+			fmt.Println(strs[1])
+		case "createConnection":
+			fmt.Println(strs[1])
+		case "stabilize":
+			fmt.Println(strs[1])
+		case "printStore":
+			fmt.Println(strs[1])
+		case "put":
+			fmt.Println(strs[1])
+		case "get":
+			fmt.Println(strs[1])
+		default:
+			fmt.Println("bad command")
+		}
+	}
+}
