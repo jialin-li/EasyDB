63182c9f53a8c9ef39f3bc4982909629849b8ca1
jialin-li <jialin_li@utexas.edu>
2018-01-31 15:07:18 -0600

    server interface - does not work

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"shared"
+)
+
+type KVServer struct {
+}
+
+// Terminate a server
+func (*KVServer) Terminate() error {
+	return nil
+}
+
+// Ask the server to connect to another server
+func (*KVServer) Connect(args *shared.Args, reply *shared.Response) error {
+	return nil
+}
+
+// Send back the current key value store
+func (*KVServer) DumpStore(args *shared.Args, reply *shared.Response) error {
+	return nil
+}
+
+// Disconnect if we are clients of any other servers
+func (*KVServer) Disconnect(args *shared.Args, reply *shared.Response) error {
+	return nil
+}
+
+// No more new write requests will be send until the next write
+func (*KVServer) Stabilize(args *shared.Args, reply *shared.Response) error {
+	return nil
+}
+
+// Put a KV pair
+func (*KVServer) Put(args *shared.Args, reply *shared.Response) error {
+	return nil
+}
+
+// Get a Value based on a key
+func (*KVServer) Get(args *shared.Args, reply *shared.Response) error {
+	return nil
+}
---
+++ server_main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net"
+	"net/rpc"
+	// "github.com/jialin-li/EasyDB/shared" //Path to the package contains shared struct
+)
+
+func main() {
+
+	//Creating an instance of struct which implement Arith interface
+	kv := new(KVServer)
+
+	// Register a new rpc server (In most cases, you will use default server only)
+	// And register struct we created above by name "Arith"
+	// The wrapper method here ensures that only structs which implement Arith interface
+	// are allowed to register themselves.
+	server := rpc.NewServer()
+	server.RegisterName("KVServer", kv)
+	// Get the port from OS args
+	// Listen for incoming tcp packets on specified port.
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+
+	// This statement links rpc server to the socket, and allows rpc server to accept
+	// rpc request coming from that socket.
+	server.Accept(l)
+}
