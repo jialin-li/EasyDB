90d863a0bcbbd30d666eee6cc79d56dfa6029f8b
William Lin <wlsaidhi@gmail.com>
2018-02-03 13:21:14 -0600

    added -term flag to client

--- client/client.go
+++ client/client.go
@@ -1,10 +1,55 @@
 package main
 
-import "fmt"
+import (
+	//"EasyDB/client"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+
+	// Use the -term flag to run  the client as a command line program. Client
+	// will wait for commands from stdin. Useful for debugging and for a real
+	// distributed system.
+	termPtr := flag.Bool("term", false, "run as program")
+
+	flag.Parse()
+	if *termPtr {
+		parseCommands()
+	}
+
 	client(5)
 }
+
+func parseCommands() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, _ := reader.ReadString('\n')
+
+		switch strs := strings.Split(text, " "); strs[0] {
+		case "joinClient":
+			fmt.Println(strs[1])
+		case "breakConnection":
+			fmt.Println(strs[1])
+		case "createConnection":
+			fmt.Println(strs[1])
+		case "stabilize":
+			fmt.Println(strs[1])
+		case "printStore":
+			fmt.Println(strs[1])
+		case "put":
+			fmt.Println(strs[1])
+		case "get":
+			fmt.Println(strs[1])
+		default:
+			fmt.Println("bad command")
+		}
+	}
+}
+
 func client(id int) int {
 	for i := 0; i < id; i++ {
 		fmt.Println("in client")
